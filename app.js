0256ab20bd9cc285a7356b0ffe04f7e589a78425
antonio <antoniovasa07@gmail.com>
2025-02-15 17:52:56 -0600

    se actualizo index

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaNomnres = [];
+
+
+    function agregarAmigo() {
+        let nomnbreDeTexto = document.getElementById('amigo').value;
+        let nomnbreDeTextoLista = document.getElementById('listaAmigos').value;
+        if (nomnbreDeTexto === ""){
+                alert("Error, Por favor, inserte un nombre.")
+                return
+        }
+        listaNomnres.push(nomnbreDeTexto);
+        console.log("hola soy toño");
+       // limpiarCajadeTexto();
+    }
+/*
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function limpiarCajadeTexto() {
+    document.querySelector('#amigo').value = '';
+}
+
+function sortearAmigo() {
+    let nombreSorteado = Math.floor(Math.random()*listaNomnres.length);
+
+//si ya sorteamos todos los numeros
+            let (nombreSorteado.length == listaNomnres)
+                
+            
+
+    }
+*/
