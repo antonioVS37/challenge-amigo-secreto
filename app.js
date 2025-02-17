6728149b527ac8c6150b0bbc9aa652f22c2c7f2a
antonio <antoniovasa07@gmail.com>
2025-02-17 16:07:20 -0600

    codigo terminado

--- app.js
+++ app.js
@@ -4,34 +4,41 @@ let listaNomnres = [];
 
 
     function agregarAmigo() {
-        let nomnbreDeTexto = document.getElementById('amigo').value;
-        let nomnbreDeTextoLista = document.getElementById('listaAmigos').value;
+        let nomnbreDeTexto = document.getElementById('amigo');
+        let nomnbreDeTextoLista = document.getElementById('listaAmigos');
         
-        if (nomnbreDeTexto === ""){
-                alert("Error, Por favor, inserte un nombre.")
-                return
+        if (nomnbreDeTexto.value == ""){
+            //mensaje de que no se ha insertado nada
+                alert("Error, Por favor, inserte un nombre.");
         }
-        listaNomnres.push(nomnbreDeTexto);
-        
-        let listaAmigos = document.getElementById('listaAmigos');
-        let li = document.createElement('li');
-        li.textContent = nomnbreDeTexto;
-        listaAmigos.appendChild(li);
-        
+        // puja lo agregado en la caja de texto
+        listaNomnres.push(nomnbreDeTexto.value);
+        //muestra la lista del array
+       
+        nomnbreDeTextoLista.innerHTML += `<li>${nomnbreDeTexto.value}</li>`;
+ // Limpiar caja de texto al agregar
         limpiarCajadeTexto();
-    }
+    };
 
 function limpiarCajadeTexto() {
+    //funcion de borrar caja de texto
     document.querySelector('#amigo').value = '';
 }
 
 function sortearAmigo() {
+    
     let resultadoAmigo = document.getElementById('resultado');
-
+    //busca un numero aleatoria en un rango
     let nombreSorteado = Math.floor(Math.random()*listaNomnres.length);
-
+    //elige un nombre sorteado
     let amigoSecreto = listaNomnres[nombreSorteado];    
+    //muestra el resultado
     resultadoAmigo.innerHTML = `<li>el ganador es: ${amigoSecreto}</li>`;
 
     }
 
+    //function reiniciarJuego() {
+   //     limpiarCaja();
+    //    nomnbreDeTexto = sortearAmigo();
+    //    document.getElementById('reiniciar').setAttribute('disabled','true');
+    //}
